0976a62e7bc8299243bcfd95204e9e352a5a9dd4
Ihor Borkaliuk <happyviolence1990@gmail.com>
2022-10-15 13:23:43 +0200

    small fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import { ContactList } from './ContactsList/ContactsList';
 import { Filter } from './Filter/Filter';
 import { Title } from './ContactsList/ContactsListStyled';
 
-const commonUsers = [
+const commonContacts = [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
@@ -15,7 +15,7 @@ const commonUsers = [
 
 export class App extends Component {
   state = {
-    contacts: commonUsers,
+    contacts: commonContacts,
     filter: '',
   };
 
